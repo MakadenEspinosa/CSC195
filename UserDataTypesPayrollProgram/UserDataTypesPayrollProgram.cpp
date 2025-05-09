214a64ed37523758a0c3f40bacd7c394748cb9a8
Makaden Espinosa <mespinosa@student.neumont.edu>
2025-05-09 08:00:49 -0600

    User Data Types Assignment

---
+++ UserDataTypesPayrollProgram/UserDataTypesPayrollProgram.cpp
@@ -0,0 +1,26 @@
+// UserDataTypesPayrollProgram.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Employee.h"
+
+int main() {
+    int numEmployees;
+    std::cout << "Number of employees (max 5): ";
+    std::cin >> numEmployees;
+
+    if (numEmployees > 5) numEmployees = 5; // Limit to 5 employees
+
+    Employee employees[5]; // Array of Employee objects
+
+    for (int i = 0; i < numEmployees; i++) {
+        std::cout << "\nEnter details for Employee " << (i + 1) << ":\n";
+        employees[i].Read();
+    }
+
+    std::cout << "\n--- Payroll Summary ---\n";
+    for (int i = 0; i < numEmployees; i++) {
+        employees[i].Write();
+    }
+    return 0;
+}
