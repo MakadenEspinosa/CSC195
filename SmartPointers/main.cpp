8583e6e4aec5e9b9655784adb4592d40921089ae
Makaden Espinosa <mespinosa@student.neumont.edu>
2025-06-02 16:15:08 -0600

    Smart Pointers Notes

---
+++ SmartPointers/main.cpp
@@ -0,0 +1,59 @@
+#define _CRT  DBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#include <iostream>
+
+#include<memory>
+using namespace std;
+
+template<typename T>
+class smart_ptr
+{
+public:
+	smart_ptr(T* ptr = nullptr) : _ptr{ ptr } {}
+	smart_ptr(const smart_ptr&) = delete;
+	smart_ptr& operator = (const smart_ptr&) = delete;
+
+	~smart_ptr() { if (_ptr) delete _ptr; }
+
+	T& operator * () { return *_ptr; }
+	T* operator & () { return  _ptr; }
+	T* operator -> () { return  _ptr; }
+
+	T* get() { return _ptr; }
+
+private:
+	T* _ptr{};
+};
+
+int main() {
+	int a[] = { 1, 2, 3, 4, 5 };
+
+	int* p = a;//int* p = &a[0];
+
+	cout << *p << endl;
+	//pointer arithmetic
+	p++;
+	p++;
+	p--;
+	p = p + 2;
+
+	cout << *p << endl;
+
+	int* ptHeap = new int;
+
+	cout << "========================" << endl;
+	cout << ptHeap << endl;
+	cout << *ptHeap << endl;
+
+	delete ptHeap;
+	{
+		smart_ptr<int> sp(new int{ 5 });
+		//smart_ptr<int> spc(sp); -- won't be possible, copy constructor disabled
+		unique_ptr<int> up(new int{ 10 });
+	}
+	
+
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
